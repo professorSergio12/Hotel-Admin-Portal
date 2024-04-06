004dfd0a954cf8ab2377109a6651e5891c91abdd
Rohit Rawat <107769619+professorSergio12@users.noreply.github.com>
2024-04-06 22:21:27 +0530

    Add files via upload

---
+++ public/js/form.js
@@ -0,0 +1,17 @@
+function showBookingForm() {
+    var bookingForm = document.getElementById('form-body-id');
+    bookingForm.style.display = 'flex';
+}
+
+
+function hideBookingForm() {
+    var formhide = document.getElementById('form-body-id');
+    formhide.style.display = 'none';
+}
+
+//Booking 
+
+function hideForm() {
+    var formhide = document.getElementById('edit-form');
+    formhide.style.display = 'none';
+}
